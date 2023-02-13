12777b7ab3ed62288903077808b82f954df215a0
kucin <kucin@LAPTOP-2VJU9K9D>
2023-02-13 01:14:07 +0100

    Zaliczenie lab3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,22 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+
+    unsigned int temp_towar = 0; 
+    unsigned int zaglowce_liczba = 0;
+
+    while (temp_towar < towar) {
+        Stocznia stocznia{};
+        Statek*  st1 = stocznia();
+        temp_towar += st1->transportuj();
+
+        if ((dynamic_cast< Zaglowiec* >(st1)) != NULL) {
+            zaglowce_liczba += 1;
+
+            delete st1;
+        }
+    }
+
+
+    return zaglowce_liczba;
 }
